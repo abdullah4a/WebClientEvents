dfa28376a663b064b750b983ee3e88b7be8a18ca
wa_ab <waqas@itinsync.com>
2022-03-20 11:05:56 +0500

    Initial commit

---
+++ src/main/java/SpringWebClients.java
@@ -0,0 +1,48 @@
+import model.Events;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+public class SpringWebClients {
+    private final WebClient webClient;
+
+    public SpringWebClients() {
+        this.webClient = WebClient.builder()
+                .baseUrl("http://localhost:8080/Events")
+                .build();
+    }
+
+    public static void main(String[] args) {
+        SpringWebClients api = new SpringWebClients();
+
+        api.postNewEvents()
+                .thenMany(api.getAllProducts())
+                .take(1)
+                .subscribeOn(Schedulers.newSingle("myThread"))
+                .subscribe();
+
+        try {
+            Thread.sleep(5000);
+        } catch (Exception ignored) {
+        }
+    }
+
+    private Mono<ResponseEntity<Events>> postNewEvents() {
+        return webClient
+                .post()
+                .body(Mono.just(new Events(0L, "Test", "Test", "Test")), Events.class)
+                .exchangeToMono(response -> response.toEntity(Events.class))
+                .doOnSuccess(o -> System.out.println("POST " + o));
+    }
+
+    private Flux<Events> getAllProducts() {
+        return webClient
+                .get()
+                .retrieve()
+                .bodyToFlux(Events.class)
+                .doOnNext(o -> System.out.println("GET: " + o));
+    }
+
+}
---
+++ src/main/java/model/Events.java
@@ -0,0 +1,17 @@
+package model;
+
+
+public class Events {
+    private Long id;
+    private String title;
+    private String price;
+    private String remark;
+
+    public Events(long id, String title, String price, String remark) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.remark = remark;
+
+    }
+}
